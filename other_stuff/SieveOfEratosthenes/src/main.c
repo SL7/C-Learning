26c2a7fd7b31cb84145defb6553b07b188d8319d
SL7 <sl7@flate.io>
2021-03-22 22:12:46 +0100

    updated sieve of eratosthenes

--- other_stuff/SieveOfEratosthenes/src/main.c
+++ other_stuff/SieveOfEratosthenes/src/main.c
@@ -6,39 +6,39 @@
 void sieve_of_eratosthenes(int n);
 
 int main(void) {
-    int msec = 0;
-    time_t start = clock();
     sieve_of_eratosthenes(1000000);
-    clock_t diff = clock() - start;
-    msec = diff * 1000 / CLOCKS_PER_SEC;
-    printf("\n\n[+] took %d seconds\n\n", msec/1000);
-
     return EXIT_SUCCESS;
 }
 
 
 void sieve_of_eratosthenes(int n) {
-    int *sieve = calloc(n, sizeof(int));
-    sieve[0] = 2;
-    int count = 0;
-    for (int i = 2, k = 0; i <= n; i++) {
-        for (int j = 0; j < n; j++) {
-            if ((i/2) < sieve[j]) {
-                sieve[k++] = i;
-                
-                printf("%10d ", i);
-                count++;
-                if (count == 8) {
-                    printf("\n");
-                    count = 0;
-                }
-                break;
-            }
-            if (i % sieve[j] == 0) {
-                break;
+    int *sieve = calloc(n+1, sizeof(int));
+    for(int i = 0; i <= n; i++) {
+        sieve[i] = 1;
+    }
+    sieve[0] = sieve[1] = 0;
+    int msec = 0;
+    time_t start = clock();
+    for (int i = 2; i*i <= n; i++) {
+        if(sieve[i]) {
+            for (int j = i*i; j <= n; j += i) {
+                sieve[j] = 0;
             }
         }
     }
-    printf("\n");
+    clock_t diff = clock() - start;
+    msec = diff * 1000 / CLOCKS_PER_SEC;
+    for(int i = 0, count = 0;i <= n; i++) {
+        if (sieve[i]) {
+            printf("%10d ", i);
+            if (count == 8) {
+                count = 0;
+                printf("\n");
+                continue;
+            }
+            count++;
+        }
+    }
+    printf("\n\n[+] took %d milliseconds\n\n", msec%1000);
     free(sieve);
 }
